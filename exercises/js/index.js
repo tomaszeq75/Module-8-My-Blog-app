154d7b89c068c80925820c70398f273e81490657
Tomek <t.wolyniec@tlen.pl>
2020-10-09 12:55:23 +0200

    6:57

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -1,10 +1,25 @@
+// const { response } = require("express");
+
 const API_URL = "http://localhost:3000/api/posts";
+// const API_URL = "http://localhost:3000/api/posts/1581461442206";
 const API_BASE_URL = "http://localhost:3000/";
 
 window.onload = () => {
     getPosts();
 };
 
-const getPosts = () => {};
+const getPosts = () => {
+    fetch(API_URL, {
+        method: "GET",
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return buildPosts(data);
+        });
+};
 
-const buildPosts = (blogPosts) => {};
+const buildPosts = (blogPosts) => {
+    console.log(blogPosts);
+};
